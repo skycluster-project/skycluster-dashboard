32ebb44fa5789feb620434712ffa7dec96488164
Ehsan <eh.etesami@gmail.com>
2024-08-12 22:36:21 +0000

    Modify CRs pages to include chip for various actions

--- pkg/frontend/src/pages/CRsPage.tsx
+++ pkg/frontend/src/pages/CRsPage.tsx
@@ -1,5 +1,5 @@
-import { Info as InfoIcon} from '@mui/icons-material';
-import {Alert, Grid, LinearProgress, Chip, Paper, Typography} from "@mui/material";
+import { Info as InfoIcon, HelpOutline as HelpOutlineIcon, DeleteForever as DeleteForeverIcon} from '@mui/icons-material';
+import {Divider, Alert, Box, Grid, LinearProgress, Chip, Paper, Typography} from "@mui/material";
 import {useParams} from "react-router-dom";
 import {K8sResource, CRD, ItemList} from "../types.ts";
 import {useEffect, useState} from "react";
@@ -13,6 +13,12 @@ const CRDPage = () => {
     const [crd, setCRD] = useState<CRD | null>(null);
     const [crs, setCRs] = useState<ItemList<K8sResource> | null>(null);
     const [error, setError] = useState<object | null>(null);
+    
+    const copyToClipboard = (name: string) => {
+        navigator.clipboard.writeText(name).then(() => {}, (err) => {
+            console.error('Could not copy text: ', err);
+        });
+    };
 
     useEffect(() => {
         apiClient.getCRD(crdName as string + "." + crdGroup as string)
@@ -35,9 +41,12 @@ const CRDPage = () => {
         return <LinearProgress/>
     }
 
+    // create a list of all cr.metadata.name separated by spaces
+    const crNames = crs.items.map((cr) => cr.metadata.name).join(" ");
+
     return (
         <>
-            <HeaderBar title={crd.metadata.name} super="CRD"/>
+            <HeaderBar title={crd.spec.names.kind} super="CRD"/>
             <PageBody>
                 <Grid container spacing={2} alignItems="stretch">
                     <Grid item xs={12} md={6}>
@@ -45,18 +54,37 @@ const CRDPage = () => {
                             <Typography variant="h6">{crd.spec.names.kind}</Typography>
                             <Typography variant="body1" display="inline">{crd.metadata.name}</Typography>
                             <Chip sx={{ p: 0, mt: 0.5, ml: 1, '& > *': { ml: '8px !important', mr: '-8px !important' } }}
-                                icon={<InfoIcon />} size="small" variant="outlined" color="primary" />
+                                icon={<InfoIcon />} size="small" variant="outlined" color="primary"
+                                onClick={() => copyToClipboard("kubectl get " + crd.spec.names.kind + " | less")} />
                             <ConditionChips status={crd.status} />
+                            <Chip variant="outlined" className="mr-1 ml-1"
+                                key={"deleteAll"}
+                                label={"Delete All"}
+                                title={"Delete ALL"}
+                                color={("error")} 
+                                onClick={() => copyToClipboard("kubectl get delete " + crd.spec.names.kind + " " + crNames)}></Chip>
                         </Paper>
                     </Grid>
-                    {crs.items.map((cr) => (
-                        <Grid item xs={12} key={cr.metadata.name}>
-                            <Paper className="p-4">
-                                <Typography variant="h6">{cr.metadata.name}</Typography>
-                                <Typography variant="body1">{JSON.stringify(cr.metadata)}</Typography>
-                            </Paper>
-                        </Grid>
-                    ))}
+                    <Grid item xs={12} md={6}>
+                        <Paper className="p-3">
+                        {crs.items.map((cr) => (
+                        <Box sx={{display: 'flex', flexDirection: 'row', p: 0, m: 0}} key={cr.metadata.name}>
+                            <Typography variant="h6">{cr.metadata.name}</Typography>
+                                <Box sx={{display: 'flex', flexDirection: 'row', p: 0, m: 0}}>
+                                    <Chip sx={{ p: 0, mt: 0.5, ml: 1, '& > *': {ml: '8px !important', mr: '-8px !important',}, }}
+                                        icon={<InfoIcon />} size="small" variant="outlined" color="primary"
+                                        onClick={() => copyToClipboard("kubectl get " + cr.kind + " " + cr.metadata.name)} />
+                                    <Chip sx={{ p: 0, mt: 0.5, ml: 1, '& > *': {ml: '8px !important', mr: '-8px !important',}, }}
+                                        icon={<HelpOutlineIcon />} size="small" variant="outlined" color="secondary"
+                                        onClick={() => copyToClipboard("kubectl get -o yaml " + cr.kind + " " + cr.metadata.name + " | less -SRF")} />
+                                    <Chip sx={{ p: 0, mt: 0.5, ml: 1, '& > *': {ml: '8px !important', mr: '-8px !important',}, }}
+                                        icon={<DeleteForeverIcon />} size="small" variant="outlined" color="error"
+                                        onClick={() => copyToClipboard("kubectl delete " + cr.kind + " " + cr.metadata.name)} />
+                                </Box>
+                        </Box>
+                        ))}
+                        </Paper>
+                    </Grid>
                 </Grid>
             </PageBody>
         </>
